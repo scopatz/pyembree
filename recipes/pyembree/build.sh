5f8ad2c021694432f3596f678e5f99a84d231af4
Anthony Scopatz <scopatz@gmail.com>
2015-05-13 21:00:14 -0500

    added c include path

--- recipes/pyembree/build.sh
+++ recipes/pyembree/build.sh
@@ -2,6 +2,7 @@
 export LD_LIBRARY_PATH=$PREFIX/lib
 export PATH=$PREFIX/bin:$PATH
 export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib/cyclus:$PREFIX/lib
+export C_INCLUDE_PATH=$PREFIX/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=$PREFIX/include:$CPLUS_INCLUDE_PATH
 
 python setup.py install --prefix=$PREFIX
