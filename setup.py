0cb2a3119a01699b9c3ff509af05dd1d9acb6d05
Anthony Scopatz <scopatz@gmail.com>
2015-05-13 21:02:46 -0500

    find out stuff

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@
 
 #from distutils.core import setup
 from setuptools import setup, find_packages
-from distutils.extension import Extension
+#from distutils.extension import Extension
 
 import numpy as np
 from Cython.Build import cythonize
 
 include_path = [np.get_include()]
 
+ext_modules = cythonize('pyembree/*.pyx', language='c++', 
+                        include_dirs=include_path)
+print(include_path)
+print(ext_modules)
+
 setup(
     name="pyembree",
-    ext_modules=cythonize('pyembree/*.pyx', language='c++', 
-                          include_dirs=include_path),
+    ext_modules=ext_modules,
     packages=find_packages(),
 )
