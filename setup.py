69d95cfd2dd15714653cbe354d2285b2eb0f3975
Anthony Scopatz <scopatz@gmail.com>
2015-05-15 11:20:13 -0500

    install pxd files

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-#from distutils.core import setup
 from setuptools import setup, find_packages
-#from distutils.extension import Extension
 
 import numpy as np
 from Cython.Build import cythonize
@@ -19,4 +17,5 @@ setup(
     name="pyembree",
     ext_modules=ext_modules,
     packages=find_packages(),
+    package_data = {'pyembree': ['*.pxd']}
 )
