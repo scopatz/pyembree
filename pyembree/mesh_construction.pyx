f52cc20d513efbe64c089b07f739984c25af591d
atmyers <atmyers2@gmail.com>
2015-05-15 19:21:05 -0700

    adding some more docs

--- pyembree/mesh_construction.pyx
+++ pyembree/mesh_construction.pyx
@@ -8,42 +8,56 @@ from rtcore cimport Vertex, Triangle, Vec3f
 from libc.stdlib cimport malloc, free
 
 cdef class TriangleMesh:
+    r'''
+
+    This class constructs a polygon mesh with triangular elements and 
+    adds it to the scene. 
+
+    Parameters
+    ----------
+
+    scene : EmbreeScene
+        This is the scene to which the constructed polygons will be
+        added.
+    vertices : a np.ndarray of floats. 
+        This specifies the x, y, and z coordinates of the vertices in 
+        the polygon mesh. This should either have the shape 
+        (num_triangles, 3, 3), or the shape (num_vertices, 3), depending
+        on the value of the `indices` parameter.
+    indices : either None, or a np.ndarray of ints
+        If None, then vertices must have the shape (num_triangles, 3, 3).
+        In this case, `vertices` specifices the coordinates of each
+        vertex of each triangle in the mesh, with vertices being 
+        duplicated if they are shared between triangles. For example,
+        if indices is None, then vertices[2][1][0] should give you 
+        the x-coordinate of the 2nd vertex of the 3rd triangle.
+        If indices is a np.ndarray, then it must have the shape
+        (num_triangles, 3), and `vertices` must have the shape
+        (num_vertices, 3). In this case, indices[2][1] tells you 
+        the index of the 2nd vertex of the 3rd triangle in `indices`,
+        while vertices[5][2] tells you the z-coordinate of the 6th
+        vertex in the mesh. Note that the indexing is assumed to be
+        zero-based. In this setup, vertices can be shared between
+        triangles, and the number of vertices can be less than 3 times
+        the number of triangles.
+            
+    '''
+
     cdef Vertex* vertices
     cdef Triangle* indices
     cdef unsigned int mesh
 
-    def __init__(self, rtcs.EmbreeScene scene, 
-                 np.ndarray vertices, 
+    def __init__(self, rtcs.EmbreeScene scene,
+                 np.ndarray vertices,
                  np.ndarray indices = None):
-        '''
-
-        Constructs a triangular element mesh from a set of vertices and
-        adds them to the scene. There are two different ways of 
-        constructing the mesh:
-        
-        If "indices" is None, then "vertices" should be a num_triangles 
-        by 3 by 3 array of floats, constructed so that (for example) 
-        vertices[2][1][0] gives you the x-component of the 2nd vertex of 
-        the 3rd triangle. In general, vertices may be duplicated when 
-        constructing the mesh in this fashion. 
-        
-        Alternatively, you can also pass in an "indices" array. In that case,
-        "indices" should be a num_triangles by 3 array of ints, and "vertices" 
-        should be a num_vertices by 3 array of floats. These arrays should be 
-        constructed so that indices[2][0] returns the index in "vertices" of 
-        the first vertex of the third triangle, while vertices[4][1] returns
-        the y-coordinate of the 5th vertex. Vertices can be shared when 
-        constructing the mesh in this fashion, so that num_vertices can be 
-        less than 3 times the number of triangles. 
-        
-        '''
 
         if indices is None:
             self._build_from_flat(scene, vertices)
         else:
             self._build_from_indices(scene, vertices, indices)
 
-    cdef void _build_from_flat(self, rtcs.EmbreeScene scene, np.ndarray tri_vertices):
+    cdef void _build_from_flat(self, rtcs.EmbreeScene scene, 
+                               np.ndarray tri_vertices):
         cdef int i, j
         cdef int nt = tri_vertices.shape[0]
         # In this scheme, we don't share any vertices.  This leads to cracks,
@@ -83,30 +97,64 @@ cdef class TriangleMesh:
 
         cdef unsigned int mesh = rtcg.rtcNewTriangleMesh(scene.scene_i,
                     rtcg.RTC_GEOMETRY_STATIC, nt, nv, 1) 
-        
+
+        # set up vertex and triangle arrays. In this case, we just read
+        # them directly from the inputs
         cdef Vertex* vertices = <Vertex*> rtcg.rtcMapBuffer(scene.scene_i, mesh,
-                        rtcg.RTC_VERTEX_BUFFER)
+                                                    rtcg.RTC_VERTEX_BUFFER)
 
         for i in range(nv):
                 vertices[i].x = tri_vertices[i, 0]
                 vertices[i].y = tri_vertices[i, 1]
                 vertices[i].z = tri_vertices[i, 2]
+
         rtcg.rtcUnmapBuffer(scene.scene_i, mesh, rtcg.RTC_VERTEX_BUFFER)
 
         cdef Triangle* triangles = <Triangle*> rtcg.rtcMapBuffer(scene.scene_i,
                         mesh, rtcg.RTC_INDEX_BUFFER)
+
         for i in range(nt):
             triangles[i].v0 = tri_indices[i][0]
             triangles[i].v1 = tri_indices[i][1]
             triangles[i].v2 = tri_indices[i][2]
 
         rtcg.rtcUnmapBuffer(scene.scene_i, mesh, rtcg.RTC_INDEX_BUFFER)
+
         self.vertices = vertices
         self.indices = triangles
         self.mesh = mesh
 
 cdef class ElementMesh(TriangleMesh):
-    # This takes
+    r'''
+
+    Currently, we handle non-triangular mesh types by converting them 
+    to triangular meshes. This class performs this transformation.
+    Currently, this is implemented for hexahedral and tetrahedral
+    meshes.
+
+    Parameters
+    ----------
+
+    scene : EmbreeScene
+        This is the scene to which the constructed polygons will be
+        added.
+    vertices : a np.ndarray of floats. 
+        This specifies the x, y, and z coordinates of the vertices in 
+        the polygon mesh. This should either have the shape 
+        (num_vertices, 3). For example, vertices[2][1] should give the 
+        y-coordinate of the 3rd vertex in the mesh.
+    indices : a np.ndarray of ints
+        This should either have the shape (num_elements, 4) or 
+        (num_elements, 8) for tetrahedral and hexahedral meshes, 
+        respectively. For tetrahedral meshes, each element will 
+        be represented by four triangles in the scene. For hex meshes,
+        each element will be represented by 12 triangles, 2 for each 
+        face. For hex meshes, we assume that the node ordering is as
+        defined here: 
+        http://homepages.cae.wisc.edu/~tautges/papers/cnmev3.pdf
+            
+    '''
+
     def __init__(self, rtcs.EmbreeScene scene,
                  np.ndarray vertices, 
                  np.ndarray indices):
