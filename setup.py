57199d98346bbeb0f3ea5671fc04e19ef3954417
Guillaume Jacquenot <Gjacquenot@users.noreply.github.com>
2018-01-03 12:20:22 +0100

    Fixed a keyword error in setup.py
    Replaced `include_dir` with `include_path` that is an unknown compilation option for cythonize
    
    https://travis-ci.org/scopatz/pyembree/jobs/324534634#L843
    
        Copying /home/travis/build/scopatz/pyembree to /home/travis/miniconda/conda-bld/pyembree_1514977538913/work
        source tree in: /home/travis/miniconda/conda-bld/pyembree_1514977538913/work
        Traceback (most recent call last):
          File "setup.py", line 11, in <module>
            include_dirs=include_path)
          File "/home/travis/miniconda/conda-bld/pyembree_1514977538913/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_pl/lib/python2.7/site-packages/Cython/Build/Dependencies.py", line 909, in cythonize
            c_options = CompilationOptions(**options)
          File "/home/travis/miniconda/conda-bld/pyembree_1514977538913/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_pl/lib/python2.7/site-packages/Cython/Compiler/Main.py", line 559, in __init__
            raise ValueError(message)
        ValueError: got unknown compilation option, please remove: include_dirs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from Cython.Build import cythonize
 include_path = [np.get_include()]
 
 ext_modules = cythonize('pyembree/*.pyx', language='c++',
-                        include_dirs=include_path)
+                        include_path=include_path)
 for ext in ext_modules:
     ext.include_dirs = include_path
     ext.libraries = ["embree"]
