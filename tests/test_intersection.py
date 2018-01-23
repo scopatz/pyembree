ce483466416b6fd8119ab5e43b6a50a3f3e19f21
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2018-01-23 21:48:37 +0100

    Added a simple for tetrahedron geometry

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -3,6 +3,7 @@ import numpy as np
 from pyembree import rtcore as rtc
 from pyembree import rtcore_scene as rtcs
 from pyembree.mesh_construction import TriangleMesh
+from pyembree.mesh_construction import ElementMesh
 
 
 def xplane(x):
@@ -122,6 +123,44 @@ class TestIntersectionTrianglesFromIndices(TestCase):
         self.assertTrue(np.allclose([6.9, 6.9, 6.9], tfar))
         self.assertTrue(np.allclose([0.4, 0.1, 0.15], u))
         self.assertTrue(np.allclose([0.5, 0.4, 0.35], v))
+
+
+class TestIntersectionTetrahedron(TestCase):
+
+    def setUp(self):
+        """Initialisation"""
+        vertices = [(0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)]
+        vertices = np.array(vertices, 'float32')
+        indices = np.array([[0, 1, 2, 3]], 'uint32')
+        self.embreeDevice = rtc.EmbreeDevice()
+        self.scene = rtcs.EmbreeScene(self.embreeDevice)
+        mesh = ElementMesh(self.scene, vertices, indices)
+
+        N = 2
+        self.origins = np.zeros((N, 3), dtype='float32')
+        self.origins[0, :] = (-0.1, +0.1, +0.1)
+        self.origins[1, :] = (-0.1, +0.2, +0.2)
+        self.dirs = np.zeros((N, 3), dtype='float32')
+        self.dirs[:, 0] = 1.0
+
+    def test_intersect_simple(self):
+        res = self.scene.run(self.origins, self.dirs)
+        self.assertTrue([1, 1], res)
+
+    def test_intersect(self):
+        res = self.scene.run(self.origins, self.dirs, output=1)
+
+        self.assertTrue([0, 0], res['geomID'])
+
+        ray_inter = res['geomID'] >= 0
+        primID = res['primID'][ray_inter]
+        u = res['u'][ray_inter]
+        v = res['v'][ray_inter]
+        tfar = res['tfar'][ray_inter]
+        self.assertTrue([0, 1], primID)
+        self.assertTrue(np.allclose([0.1, 0.1], tfar))
+        self.assertTrue(np.allclose([0.1, 0.2], u))
+        self.assertTrue(np.allclose([0.1, 0.2], v))
 
 
 if __name__ == '__main__':
