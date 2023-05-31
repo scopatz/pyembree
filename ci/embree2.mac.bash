415658a1611f4bbc8bd89ddb2acf9fc45d6a0cdf
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-05-31 16:23:58 -0400

    osx

--- ci/embree2.mac.bash
+++ ci/embree2.mac.bash
@@ -12,7 +12,7 @@ curl -L -o embree.tar.gz https://github.com/embree/embree/releases/download/v2.1
 tar -zxvf embree.tar.gz
 rm embree.tar.gz
 
-cd embree-2.17.7.x86_64.macos
+cd embree-2.17.7.x86_64.macosx
 mv include/embree2 /usr/include
 mv lib/* /usr/lib
 
